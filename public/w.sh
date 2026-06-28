39b4f54cb1601a023df59d69d535cae2f45b56c7
Qamar Admin <test@example.com>
2026-06-28 19:22:15 +0300

    add: install script for whatsapp server

---
+++ public/w.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Stopping old Evolution API if it exists..."
+docker rm -f evolution-api 2>/dev/null
+echo "Starting Evolution API v1.8.3 (Lightweight version)..."
+docker run -d -p 8080:8080 --name evolution-api --restart always -e AUTHENTICATION_API_KEY=QamarAlFayhaa2026 -e AUTHENTICATION_TYPE=apikey -e CORS_ORIGIN="*" atilaalfen/evolution-api:v1.8.3
+echo "Done! Evolution API is now running in the background."
